40db8e228c0b8f630ff42e7e115e6643d49ba547
kangpengcheng <javaengineerkang@163.com>
2017-10-26 17:53:36 +0800

    number generator tools

---
+++ src/main/java/numberGenerator/App.java
@@ -0,0 +1,243 @@
+package numberGenerator;
+
+import org.apache.poi.hssf.usermodel.*;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author：PengchengKang
+ * @Description:
+ * @Date: Create in 18:09 2017/10/12
+ */
+public class App 
+{
+
+    private POIFSFileSystem fs;
+    private HSSFWorkbook wb;
+    private HSSFSheet sheet;
+    private HSSFRow row;
+
+    /**
+     * 读取Excel表格表头的内容
+     * @param is
+     * @return String 表头内容的数组
+     */
+    public String[] readExcelTitle(InputStream is) {
+        try {
+            fs = new POIFSFileSystem(is);
+            wb = new HSSFWorkbook(fs);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        sheet = wb.getSheetAt(0);
+        row = sheet.getRow(0);
+        // 标题总列数
+        int colNum = row.getPhysicalNumberOfCells();
+        System.out.println("colNum:" + colNum);
+        String[] title = new String[colNum];
+        for (int i = 0; i < colNum; i++) {
+            //title[i] = getStringCellValue(row.getCell((short) i));
+            title[i] = getCellFormatValue(row.getCell((short) i));
+        }
+        return title;
+    }
+
+    /**
+     * 读取Excel数据内容
+     * @param is
+     * @return Map 包含单元格数据内容的Map对象
+     */
+    public Map<Integer, String> readExcelContent(InputStream is) {
+        Map<Integer, String> content = new HashMap<Integer, String>();
+        String str = "";
+        try {
+            fs = new POIFSFileSystem(is);
+            wb = new HSSFWorkbook(fs);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        sheet = wb.getSheetAt(0);
+        // 得到总行数
+        int rowNum = sheet.getLastRowNum();
+        row = sheet.getRow(0);
+        int colNum = row.getPhysicalNumberOfCells();
+        // 正文内容应该从第二行开始,第一行为表头的标题
+        for (int i = 1; i <= rowNum; i++) {
+            row = sheet.getRow(i);
+            int j = 0;
+            while (j < colNum) {
+                // 每个单元格的数据内容用"-"分割开，以后需要时用String类的replace()方法还原数据
+                // 也可以将每个单元格的数据设置到一个javabean的属性中，此时需要新建一个javabean
+                // str += getStringCellValue(row.getCell((short) j)).trim() +
+                // "-";
+                str += getCellFormatValue(row.getCell((short) j)).trim() + "    ";
+                j++;
+            }
+            content.put(i, str);
+            str = "";
+        }
+        return content;
+    }
+
+    /**
+     * 获取单元格数据内容为字符串类型的数据
+     *
+     * @param cell Excel单元格
+     * @return String 单元格数据内容
+     */
+    private String getStringCellValue(HSSFCell cell) {
+        String strCell = "";
+        switch (cell.getCellType()) {
+            case HSSFCell.CELL_TYPE_STRING:
+                strCell = cell.getStringCellValue();
+                break;
+            case HSSFCell.CELL_TYPE_NUMERIC:
+                strCell = String.valueOf(cell.getNumericCellValue());
+                break;
+            case HSSFCell.CELL_TYPE_BOOLEAN:
+                strCell = String.valueOf(cell.getBooleanCellValue());
+                break;
+            case HSSFCell.CELL_TYPE_BLANK:
+                strCell = "";
+                break;
+            default:
+                strCell = "";
+                break;
+        }
+        if (strCell.equals("") || strCell == null) {
+            return "";
+        }
+        if (cell == null) {
+            return "";
+        }
+        return strCell;
+    }
+
+    /**
+     * 获取单元格数据内容为日期类型的数据
+     *
+     * @param cell
+     *            Excel单元格
+     * @return String 单元格数据内容
+     */
+    private String getDateCellValue(HSSFCell cell) {
+        String result = "";
+        try {
+            int cellType = cell.getCellType();
+            if (cellType == HSSFCell.CELL_TYPE_NUMERIC) {
+                Date date = cell.getDateCellValue();
+                result = (date.getYear() + 1900) + "-" + (date.getMonth() + 1)
+                        + "-" + date.getDate();
+            } else if (cellType == HSSFCell.CELL_TYPE_STRING) {
+                String date = getStringCellValue(cell);
+                result = date.replaceAll("[年月]", "-").replace("日", "").trim();
+            } else if (cellType == HSSFCell.CELL_TYPE_BLANK) {
+                result = "";
+            }
+        } catch (Exception e) {
+            System.out.println("日期格式不正确!");
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * 根据HSSFCell类型设置数据
+     * @param cell
+     * @return
+     */
+    private String getCellFormatValue(HSSFCell cell) {
+        String cellvalue = "";
+        if (cell != null) {
+            // 判断当前Cell的Type
+            switch (cell.getCellType()) {
+                // 如果当前Cell的Type为NUMERIC
+                case HSSFCell.CELL_TYPE_NUMERIC:
+                case HSSFCell.CELL_TYPE_FORMULA: {
+                    // 判断当前的cell是否为Date
+                    if (HSSFDateUtil.isCellDateFormatted(cell)) {
+                        // 如果是Date类型则，转化为Data格式
+
+                        //方法1：这样子的data格式是带时分秒的：2011-10-12 0:00:00
+                        //cellvalue = cell.getDateCellValue().toLocaleString();
+
+                        //方法2：这样子的data格式是不带带时分秒的：2011-10-12
+                        Date date = cell.getDateCellValue();
+                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                        cellvalue = sdf.format(date);
+
+                    }
+                    // 如果是纯数字
+                    else {
+                        // 取得当前Cell的数值
+//                        cellvalue = String.valueOf(cell.getNumericCellValue());
+                        cellvalue = String.valueOf(cell.getNumericCellValue()).replace("E8","000").replace(".","");
+                        if(cellvalue.length()==6){
+                            cellvalue+="000";
+                        }if(cellvalue.length()==8){
+                            cellvalue+="0";
+                        }
+                    }
+                    break;
+                }
+                // 如果当前Cell的Type为STRIN
+                case HSSFCell.CELL_TYPE_STRING:
+                    // 取得当前的Cell字符串
+                    cellvalue = cell.getRichStringCellValue().getString();
+                    break;
+                // 默认的Cell值
+                default:
+                    cellvalue = " ";
+            }
+        } else {
+            cellvalue = "";
+        }
+        return cellvalue;
+
+    }
+    public static void createScript(String filePath,String startNumberStr){
+        try {
+            long startNumber = Long.parseLong(startNumberStr);
+            // 对读取Excel表格内容测试
+                InputStream is2 = new FileInputStream(filePath);
+            App app = new App();
+            Map<Integer, String> map = app.readExcelContent(is2);
+            for (int i = 1; i <= map.size(); i++) {
+                String[] array = map.get(i).split("    ");
+                if(array== null || array.length==0)
+                    continue;
+                String deptName = array[0];
+                String areacode = array[1];
+                if(!deptName.equals("")){
+//                    System.out.println(deptName+"==="+areacode);
+                    System.out.println("insert \tinto \tt_sms_number \tvalues\t(uuid(),'"+areacode+"','"+startNumber+"','N','"+deptName+"');");
+                    startNumber++;
+                }else{
+                    System.out.println("=======Excel行号第【"+(i+1)+"】行数据不全，请修改后重新执行");
+                    break;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("未找到指定路径的文件!");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        //文件目录，注意文件转成.xls格式
+        String filePath="d:\\temp\\宝鸡部门.xls";
+        //开始短号
+        String startNumberStr="9651230214";
+        createScript(filePath,startNumberStr);
+
+
+    }
+}
---
+++ src/test/java/numberGenerator/Test.java
@@ -0,0 +1,12 @@
+package numberGenerator;
+
+/**
+ * @Author：PengchengKang
+ * @Description:
+ * @Date: Create in 18:09 2017/10/12
+ */
+public class Test {
+    public static void main(String[] args) {
+        App app = new App();
+    }
+}
